d1f2b53faece34037bcad571d4cf1f06bddf43d8
Anastassios Martakos <anastassios.martakos@gmail.com>
2017-01-17 19:27:04 +0100

    Corrected redirection chain

--- lib/graph.py
+++ lib/graph.py
@@ -17,12 +17,15 @@ class Graph(nx.DiGraph):
                 self.__internal_nodelist[host] = resp
 
         if n.history:
-            last_resp = n
+            last_resp = None
             for resp in n.history:
                 host = self.__get_hostname(resp.url)
                 __internal_add_node(resp)
-                self.add_edge(host, self.__get_hostname(last_resp.url))
+                if last_resp is not None:
+                    self.add_edge(self.__get_hostname(last_resp.url), host)
                 last_resp = resp
+
+            self.add_edge(self.__get_hostname(last_resp.url), self.__get_hostname(n.url))
         else:
             __internal_add_node(n)
 
